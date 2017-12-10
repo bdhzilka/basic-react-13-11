521fd328e6aa1951f0f1743d0eb7e7168a6b6cfe
Nataliia Zakharchuk <bdzhilkaboo@gmail.com>
2017-12-10 19:22:35 +0200

    - immutable comments

--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -1,16 +1,24 @@
 import { ADD_COMMENT } from '../constants'
 import {normalizedComments} from '../fixtures'
-import {arrToMap} from './utils'
+import {arrToImmutableMap} from './utils'
+import {Record} from 'immutable'
 
-export default (state = arrToMap(normalizedComments), action) => {
+
+const CommentRecord = Record({
+    id: null,
+    user: null,
+    text: null,
+})
+
+export default (state = arrToImmutableMap(normalizedComments, CommentRecord), action) => {
     const { type, payload, randomId } = action
 
     switch (type) {
         case ADD_COMMENT:
-            return {...state, [randomId]: {
-                ...payload.comment,
-                id: randomId
-            }}
+            return state.set(randomId, new CommentRecord({
+                id: randomId,
+                ...payload.comment
+            }))
     }
 
     return state
