857884bd5722499ff25a06059cd7ccabae4018a6
Nataliia Zakharchuk <bdzhilkaboo@gmail.com>
2017-12-17 17:51:11 +0200

    - fix load article by url /article/:id

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -47,7 +47,7 @@ export default (articles = new ReducerRecord(), action) => {
         case LOAD_ALL_ARTICLES + SUCCESS:
             return articles
                 .set('loading', false)
-                .set('entities', arrToImmutableMap(response, ArticleRecord))
+                .set('entities', arrToImmutableMap(response, ArticleRecord).merge(articles.entities))
 
         case LOAD_ARTICLE + START:
             return articles.setIn(['entities', payload.id, 'loading'], true)
